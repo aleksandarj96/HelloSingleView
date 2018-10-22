790d04eb2948674559026fa03f12e9e2afb76de2
alex <jelicicalex@hotmail.se>
2018-10-22 18:53:45 +0200

    Initial Commit

---
+++ HelloSingleView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloSingleView
+//
+//  Created by Aleksandar jelicic on 2018-10-22.
+//  Copyright © 2018 AleksandarJelicic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
